85f56a97983852ecc9731c2bbfb40950f72136ff
Unknown Branch <unknown.branch@gmail.com>
2024-08-27 02:47:39 +0500

    chore(deps): add @vercel/analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { FinancialDashboard } from "@/components/FinancialDashboard";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <main className="container mx-auto py-10 flex-grow overflow-y-auto">
+      <Analytics />
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-3xl font-bold">Finators</h1>
         <ThemeToggle />
